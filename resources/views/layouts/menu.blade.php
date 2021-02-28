133b42b6ee667f30cdec8d0ea700b3b9235ff409
tasya <tasya00661@gmail.com>
2021-02-28 22:07:10 +0700

    first commit

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,16 @@
+<ul class="navbar-nav ml-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="{{url('/')}}">Home
+                  <span class="sr-only">(current)</span>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/about')}}">About Us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/services')}}">Our Services</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{url('/contact')}}">Contact Us</a>
+              </li>
+            </ul>
